40bdb5c2e2ce708471a0ce42298f3e6032f6462b
Piaozhixian <pakushiken@gmail.com>
2022-09-27 05:16:26 +0900

    [Import] jest

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-}
+  compiler: (() => {
+    let compilerConfig = {
+      //styledComponentsの有効化
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        //本番環境ではReactTestingLibraryで使用するdatatestid属性を削除
+        reactRemoveProperties: { properties: ['^datatestid$'] },
+      }
+    }
+
+    return compilerConfig
+    })(),
+  }
 
 module.exports = nextConfig
