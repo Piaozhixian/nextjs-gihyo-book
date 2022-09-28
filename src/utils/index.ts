c876beb2f919942bc9636ef80ccf0f87ec096553
Piaozhixian <pakushiken@gmail.com>
2022-09-28 23:12:07 +0900

    [Add] fetcher

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+export const fetcher = async (
+    resource: RequestInfo,
+    init?: RequestInit,
+): Promise<any> => {
+    const res = await fetch(resource, init);
+
+    if (!res.ok) {
+        const errorRes = await res.json()
+        const error = new Error(
+            errorRes.message ?? 'API request error',
+        )
+        throw error;
+    }
+
+    return res.json();
+}
